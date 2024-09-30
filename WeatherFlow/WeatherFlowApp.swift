be3c8260c88e5f29796ea8eede27fbff72b1e34f
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2024-09-30 23:36:27 +0300

    Initial Commit

---
+++ WeatherFlow/WeatherFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherFlowApp.swift
+//  WeatherFlow
+//
+//  Created by Hüseyin Umut Kardaş on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
